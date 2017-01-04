2d88f437bd0d2e93e7c4156dcbd8b01a5477ff32
Marcin Deperas <mdeperas@future-processing.com>
2017-01-04 11:42:13 +0100

    second commit

---
+++ Market.API/Controllers/CompaniesController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using System.Web.Http;
+
+namespace Market.API.Controllers
+{
+    [RoutePrefix("api/Companies")]
+    public class CompaniesController : ApiController
+    {
+        [Authorize]
+        [Route("")]
+        public IHttpActionResult Get()
+        {
+            return Ok(Company.CreateCompanies());
+        }
+
+    }
+
+    #region Helpers
+
+    public class Company
+    {
+        public int CompanyID { get; set; }
+        public string Name { get; set; }
+        public string City { get; set; }
+
+        public static List<Company> CreateCompanies()
+        {
+            List<Company> CompanyList = new List<Company>
+            {
+                new Company {CompanyID = 10248, Name = "Taiseer Joudeh", City = "Amman" },
+                new Company {CompanyID = 10249, Name = "Ahmad Hasan", City = "Dubai" },
+                new Company {CompanyID = 10250,Name = "Tamer Yaser", City = "Jeddah" },
+                new Company {CompanyID = 10251,Name = "Lina Majed", City = "Abu Dhabi"},
+                new Company {CompanyID = 10252,Name = "Yasmeen Rami", City = "Kuwait"}
+            };
+
+            return CompanyList;
+        }
+    }
+
+    #endregion
+}
